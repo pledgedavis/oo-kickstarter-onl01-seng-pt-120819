6a951a13925a7c84f8ba2cf86d8be3e037d06d98
Pledge Davis <pledgedavis14@gmail.com>
2020-01-25 16:40:13 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Backer
-  @@backed_projects = []
+  @@projects = []
 attr_reader :name
 def initialize (name)
 @name = name 
-@@backed_projects << self
+@@backed_projects = backed_projects
 end
 
   
